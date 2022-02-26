720485af816e376a5fe278d24d6f2307b9f5a1a7
AnjaniGourisaria <82677043+AnjaniGourisaria@users.noreply.github.com>
2022-02-26 22:58:19 +0530

    model update

--- backend/api/apisetup/admin.py
+++ backend/api/apisetup/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 from .models import Student
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['id','name','roll','city']
--- backend/api/apisetup/models.py
+++ backend/api/apisetup/models.py
@@ -1,6 +1,43 @@
 from django.db import models
 
-class Student(models.Model):
-    name = models.CharField(max_length=200)
-    roll = models.IntegerField()
-    city = models.CharField(max_length=50)
+class Consumer(models.Model):
+    user = 
+    phone = 
+    city = 
+    state = 
+    zipcode =
+    publish_date = 
+    update_date = 
+    is_deleted =  
+    otp = 
+    local_ip = 
+    browser =
+    router_ip = 
+    address = 
+    password = 
+    #Phone Only
+    mac_address =
+    
+    def __str__(self):
+        return str(self.id)
+    
+    @property
+    def get_id(self):
+        return str(self.id)
+    
+
+    
+class Contacts(models.Model):
+    username = models.CharField(max_length=15,default="")
+    email = models.EmailField(max_length=30,default="")
+    phone = models.IntegerField(default="")
+    msg = models.CharField(max_length=900,default="")
+    publish_date= models.DateTimeField(default=datetime.now,blank=True)
+    update_date= models.DateTimeField(default=datetime.now,blank=True)
+    is_deleted = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return str(self.id)
+ 
+class 
+    
