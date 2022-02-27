1209e6bf63483dd3347c4c2af85165013b996816
AnjaniGourisaria <82677043+AnjaniGourisaria@users.noreply.github.com>
2022-02-28 02:47:59 +0530

    Model update

--- backend/api/apisetup/admin.py
+++ backend/api/apisetup/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-from .models import Student
+from .models import Contacts
+from .models import Consumer
+
+admin.site.register(Contacts)
+
+admin.site.disable_action('delete_selected')
+
+@admin.action(description='Delete')
+def is_deleted_true(modeladmin, request, queryset):
+    queryset.update(is_deleted='True')
+
+# @admin.action(description='Restore Soft')
+# def is_deleted_false(modeladmin, request, queryset):
+#     queryset.update(is_deleted='False')
+  
+class ConsumerModelAdmin(admin.ModelAdmin):
+    list_display= ('id','user','phone','city','state','zipcode','station','address','email','publish_date')
+    list_display_links = ('id','user','phone','city','state','zipcode','station','address','email','publish_date')
+    list_filter = ('publish_date','city','state','is_deleted')
+    save_on_top = True
+    list_per_page = 20
+    search_fields = ('foreignkeyfield__user',)
+    save_on_top = True
+    list_per_page = 20
+    actions = [is_deleted_true]#,is_deleted_false]
+admin.site.register(Consumer,ConsumerModelAdmin)
 
--- backend/api/apisetup/models.py
+++ backend/api/apisetup/models.py
@@ -1,22 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.utils.html import format_html 
 
+STATION = (
+    ('Home','Home'),
+    ('Office','Office'),
+    ('Work','Works'),
+    ('Others','Others'),
+)
 class Consumer(models.Model):
-    user = 
-    phone = 
-    city = 
-    state = 
-    zipcode =
-    publish_date = 
-    update_date = 
-    is_deleted =  
-    otp = 
-    local_ip = 
-    browser =
-    router_ip = 
-    address = 
-    password = 
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    #User Manual Fields
+    phone = models.IntegerField(default=0)
+    city = models.CharField(max_length=100,default="0")
+    state = models.CharField(max_length=100,default="0")
+    zipcode = models.IntegerField(default="0")
+    station = models.CharField(choices=STATION,max_length=20,default="Home")
+    address = models.CharField(max_length=500,default="0") 
+    email = models.EmailField(max_length=100,default="null@null.com") 
+    otp = models.IntegerField(default=0,null=True)    
+    #User Personal info
+    local_ip = models.CharField(max_length=200,default=0) 
+    browser = models.CharField(max_length=200,default=0) 
+    router_ip = models.CharField(max_length=200,default=0) 
     #Phone Only
-    mac_address =
+    mac_address =models.CharField(max_length=100,default=0) 
+    #Auto Fields
+    publish_date= models.DateTimeField(default=datetime.now,blank=True)
+    update_date= models.DateTimeField('publish_date', auto_now=True)
+    is_deleted =  models.BooleanField(default=False)
     
     def __str__(self):
         return str(self.id)
@@ -33,11 +47,12 @@ class Contacts(models.Model):
     phone = models.IntegerField(default="")
     msg = models.CharField(max_length=900,default="")
     publish_date= models.DateTimeField(default=datetime.now,blank=True)
-    update_date= models.DateTimeField(default=datetime.now,blank=True)
+    update_date= models.DateTimeField('publish_date', auto_now=True)
     is_deleted = models.BooleanField(default=False)
+    reslove = models.BooleanField(default=False)
     
     def __str__(self):
         return str(self.id)
  
-class 
+ 
     
